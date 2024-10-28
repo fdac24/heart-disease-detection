fe4ec0a78048cfb2e9341e2e25fc3c126bef26dc
mikjmcc26 <mmccor23@vols.utk.edu>
2024-10-28 10:21:29 -0400

    Changing color

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Title, Label, Logo } from './components/component.jsx';
 
 function App() {
   return (
-    <div style={{ backgroundColor: '#e0e0e0', minHeight: '100vh' }}>
+    <div style={{ backgroundColor: '#ECECEC', minHeight: '100vh' }}>
       {/* Render the MainForm component here */}
       <Title />
       <Label />
